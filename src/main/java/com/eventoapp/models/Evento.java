25ac906ebea298682e949026a5e614842dc11feb
Matheus Lascalla <matheusalg.nb@gmail.com>
2022-02-20 15:23:23 -0400

    Criando Mysql database

--- src/main/java/com/eventoapp/models/Evento.java
+++ src/main/java/com/eventoapp/models/Evento.java
@@ -1,8 +1,30 @@
 package com.eventoapp.models;
 
-public class Evento {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Evento implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long codigo;
 
   private String nome;
+
+  public long getCodigo() {
+    return codigo;
+  }
+
+  public void setCodigo(long codigo) {
+    this.codigo = codigo;
+  }
+
   private String local;
   private String data;
   private String horario;
